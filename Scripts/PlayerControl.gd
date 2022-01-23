4c03626d42b8477500eaaa9b086384d8644f0727
Lucas <lucasnogueira064@gmail.com>
2022-01-23 16:32:49 -0300

    Vam bora

---
+++ Scripts/PlayerControl.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+var vel = 150
+onready var sprite = $Position2D/Sprite	
+onready var position2D = $Position2D
+var FPS = 60
+var FPS_counter = 0
+
+func _ready():
+	pass 
+	
+func update_frame(limit):
+	if (FPS_counter > limit):
+		sprite.frame = (sprite.frame + 1)%4
+		FPS_counter = 0
+	else:
+		FPS_counter += 1
+	
+func _physics_process(delta):
+	update_frame(6)
+			
+	var mov  = Vector2()
+	if Input.is_action_pressed("esq"):
+		mov.x -= 1
+		update_frame(4)
+		position2D.scale.x = -1
+	if Input.is_action_pressed("dir"):
+		mov.x += 1
+		update_frame(4)
+		position2D.scale.x = 1
+	if Input.is_action_pressed("cima"):
+		mov.y -= 1
+		update_frame(4)
+	if Input.is_action_pressed("baixo"):
+		mov.y += 1
+		update_frame(4)
+
+	mov = mov.normalized()
+	mov = move_and_slide(mov*vel);
+	
