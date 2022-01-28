42cd9876b53c34d46712ee2a3c92176442f45a9b
Lucas <lucasnogueira064@gmail.com>
2022-01-28 13:14:33 -0300

    Pao

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -9,16 +9,8 @@ extends MarginContainer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"/root/AudioManager"._playMenuMusic()
-	$Menu/Tween.interpolate_property($Menu/MarginContainer/Player, "rotation_degrees", -180, 180, 4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Menu/Tween.interpolate_property($Menu/MarginContainer/Police, "rotation_degrees", 180, -180, 4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Menu/Tween.interpolate_property($Menu/MarginContainer/FBI, "rotation_degrees", -180, 180, 4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Menu/Tween.interpolate_property($Menu/MarginContainer/Reporter, "rotation_degrees", 180, -180, 4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Menu/Tween.interpolate_property($Menu/Path2D/PathFollow2D, "unit_offset", 0, 1, 50, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Menu/Tween.start()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_Jogar_pressed():
